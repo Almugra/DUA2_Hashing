5b3485c406dabd65d79c717bc9811e12cafa4172
muz <muiz-erem@gmx.de>
2023-06-10 18:08:47 +0200

    test with HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{collections::HashSet, env, time::Instant};
 
 fn main() {
     let mut args = env::args();
@@ -8,8 +8,29 @@ fn main() {
         panic!("Missing file path!");
     };
 
-    let contents = std::fs::read_to_string(path)
+    let mut map: HashSet<&str> = HashSet::new();
+    let now = Instant::now();
+    std::fs::read_to_string(path)
         .expect("Should have been able to read the file!")
         .lines()
-        .skip(1);
+        .skip(1)
+        .for_each(|line| {
+            let split: Vec<&str> = line.split(' ').collect();
+
+            match split[0] {
+                "ins" => {
+                    println!("{} {}", split[0], map.insert(split[1]));
+                }
+                "del" => {
+                    println!("{} {}", split[0], map.remove(split[1]));
+                }
+                "search" => {
+                    println!("{} {}", split[0], map.get(split[1]).is_some());
+                }
+                _ => unreachable!(),
+            }
+        });
+
+    println!("Time: {:?}", now.elapsed());
+    //./task/pubInst/b08192.txt = Time: 28.586461ms HashSet 24ms best/33ms worst
 }
