07fa17983cc7c7cd1bfe9a752e24ddcc83d54228
muz <muiz-erem@gmx.de>
2023-06-10 18:19:27 +0200

    basic custom hash function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-use std::{collections::HashSet, env, time::Instant};
+use std::collections::HashSet;
+use std::hash::Hash;
+use std::hash::Hasher;
+use std::{env, time::Instant};
+
+#[derive(PartialEq, Eq)]
+struct MyStr<'a>(&'a str);
+
+impl<'a> Hash for MyStr<'a> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.len().hash(state);
+    }
+}
 
 fn main() {
     let mut args = env::args();
@@ -8,7 +20,7 @@ fn main() {
         panic!("Missing file path!");
     };
 
-    let mut map: HashSet<&str> = HashSet::new();
+    let mut map: HashSet<MyStr> = HashSet::new();
     let now = Instant::now();
     std::fs::read_to_string(path)
         .expect("Should have been able to read the file!")
@@ -19,18 +31,20 @@ fn main() {
 
             match split[0] {
                 "ins" => {
-                    println!("{} {}", split[0], map.insert(split[1]));
+                    println!("{} {}", split[0], map.insert(MyStr(split[1])));
                 }
                 "del" => {
-                    println!("{} {}", split[0], map.remove(split[1]));
+                    println!("{} {}", split[0], map.remove(&MyStr(split[1])));
                 }
                 "search" => {
-                    println!("{} {}", split[0], map.get(split[1]).is_some());
+                    println!("{} {}", split[0], map.get(&MyStr(split[1])).is_some());
                 }
                 _ => unreachable!(),
             }
         });
 
     println!("Time: {:?}", now.elapsed());
-    //./task/pubInst/b08192.txt = Time: 28.586461ms HashSet 24ms best/33ms worst
+    //./task/pubInst/b08192.txt = Time: ~28ms HashSet
+    //./task/pubInst/b08192.txt = Time: ~35ms BTreeSet
+    //./task/pubInst/b08192.txt = Time: ~380ms CustomHashFunction
 }
