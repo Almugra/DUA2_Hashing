32b74e43fc62b5e3d83396c86bce13dcec32d442
muz <muiz-erem@gmx.de>
2023-06-10 17:31:44 +0200

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,15 @@
+use std::env;
+
 fn main() {
+    let mut args = env::args();
+    args.next();
+
+    let Some(path) = args.next() else {
+        panic!("Missing file path!");
+    };
+
+    let contents = std::fs::read_to_string(path)
+        .expect("Should have been able to read the file!")
+        .lines()
+        .skip(1);
 }
