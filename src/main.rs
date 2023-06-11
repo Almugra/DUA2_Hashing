27ff1ce64f9dec56405f7aab283385cdf1ada55e
muz <muiz-erem@gmx.de>
2023-06-11 02:39:36 +0200

    custom hashing + hashtable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashSet;
-use std::hash::Hash;
-use std::hash::Hasher;
-use std::{env, time::Instant};
+#![allow(non_snake_case)]
+use std::cell::RefCell;
+use std::collections::LinkedList;
+use std::env;
+use std::time::Instant;
 
 const CHAR_TO_NUM: [u64; 256] = {
     let mut map = [0; 256];
@@ -13,16 +14,69 @@ const CHAR_TO_NUM: [u64; 256] = {
 };
 
 #[derive(PartialEq, Eq)]
-struct MyStr<'a>(&'a str);
+struct HashTable<'a> {
+    map: Vec<RefCell<LinkedList<&'a str>>>,
+    size: usize,
+}
 
-impl<'a> Hash for MyStr<'a> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        let mut value = 0u64;
-        for byte in self.0.bytes() {
-            value *= 4;
-            value += CHAR_TO_NUM[byte as usize]
+impl<'a> HashTable<'a> {
+    fn hashCreate(n: usize) -> Self {
+        Self {
+            map: vec![RefCell::default(); n],
+            size: n,
         }
-        value.hash(state);
+    }
+
+    fn hashInsert(&mut self, x: &'a str) -> bool {
+        let index = self.hash(x);
+        let Some(list) = self.map.get(index) else {
+            unreachable!()
+        };
+
+        if list.borrow().contains(&x) {
+            false
+        } else {
+            list.borrow_mut().push_back(x);
+            true
+        }
+    }
+
+    fn hashRemove(&mut self, x: &'a str) -> bool {
+        let index = self.hash(x);
+        let Some(list) = self.map.get(index) else {
+            unreachable!()
+        };
+
+        let mut state = false;
+        let mut new_list = LinkedList::new();
+
+        for y in list.borrow().iter() {
+            if &x == y {
+                state = true;
+            } else {
+                new_list.push_back(*y);
+            }
+        }
+        self.map[index] = RefCell::new(new_list);
+        state
+    }
+
+    fn hashSearch(&mut self, x: &'a str) -> bool {
+        let index = self.hash(x);
+        let Some(list) = self.map.get(index) else {
+            unreachable!()
+        };
+
+        list.borrow().contains(&x)
+    }
+
+    fn hash(&self, key: &str) -> usize {
+        let mut value = 0u64;
+        for byte in key.bytes() {
+            value *= 4;
+            value += CHAR_TO_NUM[byte as usize];
+        }
+        value as usize % self.size
     }
 }
 
@@ -34,7 +88,7 @@ fn main() {
         panic!("Missing file path!");
     };
 
-    let mut map: HashSet<MyStr> = HashSet::new();
+    let mut map: HashTable = HashTable::hashCreate(8192);
     let now = Instant::now();
     std::fs::read_to_string(path)
         .expect("Should have been able to read the file!")
@@ -45,13 +99,13 @@ fn main() {
 
             match split[0] {
                 "ins" => {
-                    println!("{} {}", split[0], map.insert(MyStr(split[1])));
+                    println!("{} {}", split[0], map.hashInsert(split[1]));
                 }
                 "del" => {
-                    println!("{} {}", split[0], map.remove(&MyStr(split[1])));
+                    println!("{} {}", split[0], map.hashRemove(split[1]));
                 }
                 "search" => {
-                    println!("{} {}", split[0], map.get(&MyStr(split[1])).is_some());
+                    println!("{} {}", split[0], map.hashSearch(split[1]));
                 }
                 _ => unreachable!(),
             }
@@ -62,5 +116,6 @@ fn main() {
 
 //./task/pubInst/b08192.txt = Time: ~28ms HashSet
 //            "             = Time: ~35ms BTreeSet
-//            "             = Time: ~380ms CustomHashFunction
-//            "             = Time: ~30ms CustomHashByteFunction
+//            "             = Time: ~380ms Custom Hashing
+//            "             = Time: ~30ms Custom Hashing with bytes
+//            "             = Time: ~29ms Custom Hashing + HashTable
