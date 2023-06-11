f8e9a66e5d22111f13cd0a849f2a23069773dc2d
muz <muiz-erem@gmx.de>
2023-06-11 13:40:53 +0200

    custom linked list

---
+++ src/hashtable.rs
@@ -0,0 +1,151 @@
+use std::{cell::RefCell, ptr};
+
+const CHAR_TO_NUM: [u64; 256] = {
+    let mut map = [0; 256];
+    map[b'a' as usize] = 0;
+    map[b'b' as usize] = 1;
+    map[b'c' as usize] = 2;
+    map[b'd' as usize] = 3;
+    map
+};
+
+#[derive(PartialEq, Eq)]
+pub struct HashTable {
+    pub map: Vec<RefCell<LinkedList<String>>>,
+    pub size: usize,
+}
+
+impl HashTable {
+    pub fn hashCreate(n: usize) -> Self {
+        Self {
+            map: vec![RefCell::default(); n],
+            size: n,
+        }
+    }
+
+    pub fn hashInsert(&mut self, x: String) -> bool {
+        let index = self.hash(&x);
+        let Some(list) = self.map.get(index) else {
+            unreachable!()
+        };
+
+        let state = !list.borrow().iter().any(|y| y == &x);
+
+        if state {
+            list.borrow_mut().push(x);
+        }
+
+        state
+    }
+
+    pub fn hashRemove(&mut self, x: String) -> bool {
+        let index = self.hash(&x);
+        let Some(list) = self.map.get(index) else {
+            unreachable!()
+        };
+
+        let mut state = false;
+        let mut new_list = LinkedList::new();
+
+        for y in list.borrow().iter() {
+            if &x == y {
+                state = true;
+            } else {
+                new_list.push(y.to_string());
+            }
+        }
+        self.map[index] = RefCell::new(new_list);
+        state
+    }
+
+    pub fn hashSearch(&mut self, x: String) -> bool {
+        let index = self.hash(&x);
+        let Some(list) = self.map.get(index) else {
+            unreachable!()
+        };
+
+        list.borrow().iter().any(|y| y == &x)
+    }
+
+    fn hash(&self, key: &str) -> usize {
+        let mut value = 0u64;
+        for byte in key.bytes() {
+            value *= 4;
+            value += CHAR_TO_NUM[byte as usize];
+        }
+        value as usize % self.size
+    }
+}
+
+type Link<T> = *mut Node<T>;
+
+#[derive(PartialEq, Eq, Clone)]
+pub struct LinkedList<T> {
+    head: Link<T>,
+    tail: *mut Node<T>,
+}
+
+#[derive(PartialEq, Eq, Clone)]
+struct Node<T> {
+    element: T,
+    next: Link<T>,
+}
+
+pub struct IntoIter<T>(LinkedList<T>);
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            head: ptr::null_mut(),
+            tail: ptr::null_mut(),
+        }
+    }
+
+    pub fn push(&mut self, element: T) {
+        unsafe {
+            let new_tail = Box::into_raw(Box::new(Node {
+                element,
+                next: ptr::null_mut(),
+            }));
+
+            if !self.tail.is_null() {
+                (*self.tail).next = new_tail;
+            } else {
+                self.head = new_tail;
+            }
+
+            self.tail = new_tail;
+        }
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        unsafe {
+            Iter {
+                next: self.head.as_ref(),
+            }
+        }
+    }
+}
+
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.map(|node| {
+                self.next = node.next.as_ref();
+                &node.element
+            })
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,8 @@
 #![allow(non_snake_case)]
-use std::cell::RefCell;
-use std::collections::LinkedList;
 use std::env;
 use std::time::Instant;
-
-const CHAR_TO_NUM: [u64; 256] = {
-    let mut map = [0; 256];
-    map[b'a' as usize] = 0;
-    map[b'b' as usize] = 1;
-    map[b'c' as usize] = 2;
-    map[b'd' as usize] = 3;
-    map
-};
-
-#[derive(PartialEq, Eq)]
-struct HashTable<'a> {
-    map: Vec<RefCell<LinkedList<&'a str>>>,
-    size: usize,
-}
-
-impl<'a> HashTable<'a> {
-    fn hashCreate(n: usize) -> Self {
-        Self {
-            map: vec![RefCell::default(); n],
-            size: n,
-        }
-    }
-
-    fn hashInsert(&mut self, x: &'a str) -> bool {
-        let index = self.hash(x);
-        let Some(list) = self.map.get(index) else {
-            unreachable!()
-        };
-
-        if list.borrow().contains(&x) {
-            false
-        } else {
-            list.borrow_mut().push_back(x);
-            true
-        }
-    }
-
-    fn hashRemove(&mut self, x: &'a str) -> bool {
-        let index = self.hash(x);
-        let Some(list) = self.map.get(index) else {
-            unreachable!()
-        };
-
-        let mut state = false;
-        let mut new_list = LinkedList::new();
-
-        for y in list.borrow().iter() {
-            if &x == y {
-                state = true;
-            } else {
-                new_list.push_back(*y);
-            }
-        }
-        self.map[index] = RefCell::new(new_list);
-        state
-    }
-
-    fn hashSearch(&mut self, x: &'a str) -> bool {
-        let index = self.hash(x);
-        let Some(list) = self.map.get(index) else {
-            unreachable!()
-        };
-
-        list.borrow().contains(&x)
-    }
-
-    fn hash(&self, key: &str) -> usize {
-        let mut value = 0u64;
-        for byte in key.bytes() {
-            value *= 4;
-            value += CHAR_TO_NUM[byte as usize];
-        }
-        value as usize % self.size
-    }
-}
+pub mod hashtable;
+use crate::hashtable::HashTable;
 
 fn main() {
     let mut args = env::args();
@@ -99,13 +23,13 @@ fn main() {
 
             match split[0] {
                 "ins" => {
-                    println!("{} {}", split[0], map.hashInsert(split[1]));
+                    println!("{} {}", split[0], map.hashInsert(split[1].to_string()));
                 }
                 "del" => {
-                    println!("{} {}", split[0], map.hashRemove(split[1]));
+                    println!("{} {}", split[0], map.hashRemove(split[1].to_string()));
                 }
                 "search" => {
-                    println!("{} {}", split[0], map.hashSearch(split[1]));
+                    println!("{} {}", split[0], map.hashSearch(split[1].to_string()));
                 }
                 _ => unreachable!(),
             }
@@ -119,3 +43,4 @@ fn main() {
 //            "             = Time: ~380ms Custom Hashing
 //            "             = Time: ~30ms Custom Hashing with bytes
 //            "             = Time: ~29ms Custom Hashing + HashTable
+//            "             = Time: ~30ms Custom Hashing + HashTable + List
